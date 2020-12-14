2143644153c7bfd62ace17d3f5537e39aa2d9067
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-14 18:26:24 +0530

    [Arti Dandge] Add : Display Welcome Message

---
+++ TestMaximumUsingGenerics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestMaximumUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this program where we will be finding maximum number using generics");
+
+        }
+    }
+}
